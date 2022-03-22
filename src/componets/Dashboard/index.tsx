312c2d9c8accba28c9074129e38737db794719bc
g-bolsoni <giovanebolsoni2@gmail.com>
2022-03-21 23:43:43 -0300

    Create table list

--- src/componets/Dashboard/index.tsx
+++ src/componets/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 
 import { 
     Container
@@ -9,6 +10,7 @@ export const Dashboard: React.FC = () => {
   return (
       <Container>
           <Summary />
+          <TransactionsTable />
       </Container>
   );
 }
